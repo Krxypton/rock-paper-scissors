aaa476a352843d6acf4732944e798851a1000b61
Krxypton <krxypton@gmail.com>
2025-07-18 10:32:50 +0100

    Computer had a higher probability to choose rock over the other 2 so I fixed it

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function getComputerChoice() {
 
     let random = Math.floor(Math.random()*100);
 
-     if(random <= 33){
+     if(random < 33){
         return "Rock";
     }
     else if(random > 33 && random <= 66  ){
